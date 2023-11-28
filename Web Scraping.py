4ca9dd2dc48441eee3e95dfc7f3843c05a81d71a
LTL28769 <152255777+LTL28769@users.noreply.github.com>
2023-11-28 15:26:30 +0800

    Catch a number in a specific website

---
+++ Web Scraping.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import urllib.request
+import selenium
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import cfscrape
+from requests_html import HTMLSession
+session = HTMLSession()
+#r = session.get('https://money18.on.cc/')
+r = session.get('https://money18.on.cc/info/hk/liveinfo_quote_02382.html')
+r.html.render()
+#print(r.html.html)
+soup = BeautifulSoup(r.html.html, 'html.parser')
+#print(soup.prettify())
+filter = soup.find("div", class_="text-right stock-price")
+filter2= filter.find("span", class_="value")
+print(filter)
+print(filter2)
+result = "" + filter2.get_text()
+print(result)
+
