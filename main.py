fafa62e06f15e615edb96573eb7625c6c6bbcd22
LTL28769 <152255777+LTL28769@users.noreply.github.com>
2024-06-12 12:30:56 +0800

    DiscordBot_WebScraping.py

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ client = discord.Client()
 async def get_data():
     asession = AsyncHTMLSession()
     r = await asession.get('https://money18.on.cc/')
-    #r = await asession.get('https://money18.on.cc/info/hk/liveinfo_quote_02382.html')
     await r.html.arender()
-    # print(r.html.html)
     soup = BeautifulSoup(r.html.html, 'html.parser')
     print(soup.prettify())
     filter = soup.find("div", class_="stock stockDn")
     filter2 = filter.find("div", class_="value")
-    # print(filter)
-    #print(filter2)
     result = "" + filter2.get_text()
-    #print(result)
     return result
 
 @client.event
@@ -31,5 +26,4 @@ async def on_message(message):
 
     if message.content.startswith('恒指'):
         await message.channel.send(await get_data())
-        #await get_data()
 client.run('Discord Bot Token')
